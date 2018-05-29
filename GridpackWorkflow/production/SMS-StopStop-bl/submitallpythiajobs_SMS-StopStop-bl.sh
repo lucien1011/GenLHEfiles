00e6fe104143829c0b26c20f07e055c9c1e8b951
lucien1011 <lucien1011@gmail.com>
2018-05-30 00:44:39 +0200

    Change back to all mass points

--- GridpackWorkflow/production/SMS-StopStop-bl/submitallpythiajobs_SMS-StopStop-bl.sh
+++ GridpackWorkflow/production/SMS-StopStop-bl/submitallpythiajobs_SMS-StopStop-bl.sh
@@ -4,10 +4,9 @@ MODEL="SMS-StopStop-bl_mStop-"
 JOBS="jobs"
 
 baseOutDir=/eos/cms/store/user/klo/SUSY/RAWSIM/SMS-StopStop-bl/
-#for MPROD in {600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,}; do
-for MPROD in {1000,}; do
+for MPROD in {600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,}; do
     if [ ! -d "${baseOutDir}${MODEL}${MPROD}" ]; then
         mkdir ${baseOutDir}${MODEL}${MPROD}
     fi
-    python ${SCRIPT} ${MODEL}${MPROD} --in-dir /eos/cms/store/user/klo/SUSY/LHE/SMS-StopStop-bl/${MODEL}${MPROD}/ --slha ${JOBS}/${MODEL}${MPROD}/${MODEL}${MPROD}.slha --qcut-range 60 62 --outDir ${baseOutDir}${MODEL}${MPROD} --time 36000
+    python ${SCRIPT} ${MODEL}${MPROD} --in-dir /eos/cms/store/user/klo/SUSY/LHE/SMS-StopStop-bl/${MODEL}${MPROD}/ --slha ${JOBS}/${MODEL}${MPROD}/${MODEL}${MPROD}.slha --qcut-range 60 80 --outDir ${baseOutDir}${MODEL}${MPROD} --time 36000
 done
